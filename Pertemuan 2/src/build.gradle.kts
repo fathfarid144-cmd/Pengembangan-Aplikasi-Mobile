0c487aa260fc588584297ec85a63cc01e8e31d9b
Muhammad Fatahillah Farid <muhammad.123140203@student.itera.ac.id>
2026-09-25 14:24:21 +0700

    fix: gunakan JDK bundled Android Studio (JBR) dan hapus jvmToolchain

--- Pertemuan 2/src/build.gradle.kts
+++ Pertemuan 2/src/build.gradle.kts
@@ -27,6 +27,11 @@ tasks.test {
     useJUnitPlatform()
 }
 
-kotlin {
-    jvmToolchain(8)
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<JavaCompile> {
+    sourceCompatibility = "1.8"
+    targetCompatibility = "1.8"
 }
